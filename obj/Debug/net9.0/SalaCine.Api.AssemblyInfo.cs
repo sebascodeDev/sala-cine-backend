2e73da392d2e4db585c75168f6db872b4bd0fc9f
Sebastian Valarezo <sebastianvalarezomariscal2004@gmail.com>
2025-11-20 01:47:50 -0500

    chore(api): Crear estructura de carpetas del proyecto

--- obj/Debug/net9.0/SalaCine.Api.AssemblyInfo.cs
+++ obj/Debug/net9.0/SalaCine.Api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SalaCine.Api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6be90098290b73e648e4937ed5a41031eaddd4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3f1d47a8ec1c18b9b4cf1e85dc3855fc89c53b84")]
 [assembly: System.Reflection.AssemblyProductAttribute("SalaCine.Api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SalaCine.Api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
